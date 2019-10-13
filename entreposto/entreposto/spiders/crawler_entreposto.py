297d584a8117cb74ec79b70b7a5b5a52e300e31e
HenrikPereira <henriquemiguel.pereira@gmail.com>
2019-10-13 12:19:20 +0100

    Revert to previous version and new branch to make the spider able to scrape a list of urls instead of paginating (not possible by base url, and js)
    This reverts commit 27bc862d

--- entreposto/entreposto/spiders/crawler_entreposto.py
+++ entreposto/entreposto/spiders/crawler_entreposto.py
@@ -1,5 +1,6 @@
 import scrapy
-from scrapy.crawler import CrawlerProcess
+import pandas as pd
+
 
 class MainItem(scrapy.Item):
     mar = scrapy.Field()
@@ -14,45 +15,34 @@ class MainItem(scrapy.Item):
     foto = scrapy.Field()
     burl = scrapy.Field()
 
+
 class MySpider(scrapy.Spider):
     name = 'entrepostoauto'
     allowed_domains = ['entrepostoauto.pt']
-    start_urls = ['https://www.entrepostoauto.pt/viaturas/listagem?carType=novos&orderby=1&currentPage=%s' % page for page in range(1,12)]
+    start_urls = [link
+                  for sub in pd.read_csv(r'\Users\henri\OneDrive\Documentos\EDSA\AVD\project\dados_entreposto.csv')
+                      .values.tolist()
+                  for link in sub]
     BASE_URL = 'https://www.entrepostoauto.pt'
-    custom_settings = {'DEPTH_LIMIT': 1}
 
     def parse(self, response):
-        links = response.xpath("//*[@id='main']/section/div[2]/div[1]/div/section/ul/li/a/@href").extract()
-
-        for link in links:
-            full_url = self.BASE_URL + link
-            yield scrapy.Request(full_url, callback=self.parse_details)
-
-        while True:
-           next = self.driver.find_element_by_xpath('//*[@id="main"]/section/div[2]/div[2]/a[16]/@next')
-           try:
-               next.click()
-           except:
-               break
-
-    def parse_details(self, response):
 
         def merge(list1, list2):
             merged_list = tuple(zip(list1, list2))
             return merged_list
 
-        i = MainItem()
+        atrib = MainItem()
 
-        i['mar'] = response.xpath("//div/section[2]/div[1]/div[2]/h1/span/text()").extract()[0]
-        i['mod'] = response.xpath("//div/section[2]/div[1]/div[2]/h1/text()").extract()[0]
-        i['ver'] = response.xpath("//div/section[2]/div[1]/div[2]/p[1]/text()").extract()[0]
-        i['logo'] = self.BASE_URL + response.xpath("//section[1]/div[2]/article/div[3]/picture/img/@src").extract()[0]
-        i['link'] = response.url
-        i['burl'] = self.BASE_URL
-        i['preco'] = response.xpath("//div/section[2]/div[1]/div[2]/h2/text()[1]").extract()[0]
-        i['specs'] = merge(response.xpath("//article/ul/li/text()").extract(),
+        atrib['mar'] = response.xpath("//div/section[2]/div[1]/div[2]/h1/span/text()").extract()[0]
+        atrib['mod'] = response.xpath("//div/section[2]/div[1]/div[2]/h1/text()").extract()[0]
+        atrib['ver'] = response.xpath("//div/section[2]/div[1]/div[2]/p[1]/text()").extract()[0]
+        atrib['logo'] = self.BASE_URL + response.xpath("//section[1]/div[2]/article/div[3]/picture/img/@src").extract()[0]
+        atrib['link'] = response.url
+        atrib['burl'] = self.BASE_URL
+        atrib['preco'] = response.xpath("//div/section[2]/div[1]/div[2]/h2/text()[1]").extract()[0]
+        atrib['specs'] = merge(response.xpath("//article/ul/li/text()").extract(),
                            response.xpath("//article/ul/li/strong/text()").extract())
-        i['equip'] = response.xpath("//article[1]/div/div/div/p/text()").extract()
-        i['foto'] = response.xpath("//div[1]/div/div/article/div/@*").extract()
+        atrib['equip'] = response.xpath("//article[1]/div/div/div/p/text()").extract()
+        atrib['foto'] = response.xpath("//div[1]/div/div/article/div/@*").extract()[0:4]
 
-        yield i
+        yield atrib
